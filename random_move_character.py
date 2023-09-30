7e8acb5dc0d29751457df1154855a7f29460c7d4
ymyoon(OMEN) <ozone753@naver.com>
2023-09-30 17:19:06 +0900

    handle_events 추가

--- random_move_character.py
+++ random_move_character.py
@@ -7,3 +7,14 @@ tuk_ground = load_image('TUK_GROUND.png')
 char = load_image('animation_sheet.png')
 cursor = load_image('hand_arrow.png')
 
+def handle_events():
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
