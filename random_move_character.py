be32bb71821e3316c3663d743b8fa20c433d8c5c
ymyoon(OMEN) <ozone753@naver.com>
2023-09-30 17:15:45 +0900

    라이브러리 추가, 이미지 로드

---
+++ random_move_character.py
@@ -0,0 +1,9 @@
+from pico2d import *
+import random
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+char = load_image('animation_sheet.png')
+cursor = load_image('hand_arrow.png')
+
