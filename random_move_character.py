bda9d4ea409bfe45364604d33952e2d9b328c4dd
ymyoon(OMEN) <ozone753@naver.com>
2023-09-30 17:33:24 +0900

    함수 선언

--- random_move_character.py
+++ random_move_character.py
@@ -1,5 +1,5 @@
 from pico2d import *
-import random
+import random # randint 함수 사용
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
@@ -18,3 +18,19 @@ def handle_events():
             x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+hide_cursor()
+frame = 0
+
+def linear_move(pt1,pt2): # 랜덤 위치를 지정 할 함수
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    handle_events()
+    pass
+
+close_canvas()
